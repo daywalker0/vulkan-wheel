f3a73c0b305fc199f1a5a53c555fef9477852b2e
Maksim Arkhipov <Max1m.ar@yandex.ru>
2022-09-08 20:14:58 +0300

    0.2v

--- components/Wheel.vue
+++ components/Wheel.vue
@@ -3,17 +3,21 @@
 		<div class="wheel__wrap">
 			<img class="wheel__img" src="@/assets/images/wheel-spinner.png" alt="Wheel">
 			<img class="wheel__pointer" src="@/assets/images/pointer.png" alt="Pointer">
-			<div class="wheel__btn"><button @click="showPopup">spin</button></div>
+			<div class="wheel__btn-block"><a @click.prevent="rotateWheel" class="wheel__btn">spin</a></div>
 		</div>
-		<!-- <Popup
-			v-if="isPopVisible"
-		/> -->
 	</div>
 </template>
 
 <script>
 export default {
-
+methods: {
+		async rotateWheel() {
+			const btn = document.querySelector('.wheel__btn');
+			const spinner = document.querySelector('.wheel__img');
+			const popup = document.querySelector('.popup');
+			btn.addEventListener('click', spinner.classList.add('rotate'))
+		}
+	}
 }
 </script>
 
@@ -35,7 +39,7 @@ export default {
 			left: 42%;
 			top: 43%;
 		}
-		&__btn {
+		&__btn-block {
 			background: url('@/assets/images/ball2.png');
 			background-size: cover;
 			width: 64px;
@@ -46,16 +50,34 @@ export default {
 			top: 44%;
 			text-align: center;
 			vertical-align: middle;
-			button {
-				display: inline-block;
-				margin-top: 22px;
-				font-size: 20px;
-				color: #fff;
-				text-transform: uppercase;
-				background: none;
-				border: none;
-				cursor: pointer;
-			}
+		}
+		&__btn {
+			display: inline-block;
+			margin-top: 22px;
+			font-size: 20px;
+			color: #fff;
+			text-transform: uppercase;
+			background: none;
+			border: none;
+			cursor: pointer;
+		}
+	}
+	.rotate {
+		animation-name: rotation;
+    animation-duration: 5s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease;
+		animation-fill-mode: forwards;
+	}
+	@keyframes rotation {
+    0% {
+        transform:rotate(0deg);
+    }
+    85% {
+        transform:rotate(1050deg);
+    }
+		100% {
+				transform:rotate(1040deg);
 		}
 	}
 }
